9491cb17b7b95827ce7a9479cfeb4ea5992701e9
Jacob Palomo <jacob.palomo248@gmail.com>
2025-03-17 17:16:56 -0600

    Third commit

---
+++ src/app/tecnologia/page.tsx
@@ -0,0 +1,16 @@
+import Hero from '@/components/tecnologia/hero/Hero'
+import Main from '@/components/ui/Main'
+import Fact from '@/components/tecnologia/Fact'
+import Video from '@/components/tecnologia/Video'
+import Fairs from '@/components/tecnologia/Fairs/Fairs'
+
+export default function Technology() {
+	return (
+		<Main>
+			<Hero />
+			<Fact />
+			<Video />
+			<Fairs />
+		</Main>
+	)
+}
